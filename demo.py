aaef799d65e9edb7b369398f8bd927e6aacae6fa
Ashis Das <105511844+ashis-das@users.noreply.github.com>
2022-11-20 15:03:53 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,5 @@
+#   Find square root of a number
+#take two user input number
+num=float(input("enter the number"))
+root=(num**(1/2)) #as we all know that root means x^1/2
+print("the root of the",num ,"is", root)
